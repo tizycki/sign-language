428219007370ce289529bd957bfce8d9cbc42388
Tom Iżycki <tmizycki@gmail.com>
2020-09-13 00:02:43 +0200

    Refactor name

--- utils/video_splitter.py
+++ utils/video_splitter.py
@@ -21,6 +21,7 @@ def video_to_images(video_path, output_dir, file_name_prefix, output_fps):
     video_name = f"{os.path.basename(video_path).split('.')[0].replace(' ', '_')}_fps{int(output_fps):02d}"
     output_path = os.path.join(output_dir, video_name)
 
+    # Create or erase sub-folder for results
     if not os.path.exists(output_path):
         os.mkdir(output_path)
         print(f"Create output sub-folder: {video_name} in output_directory: {output_dir}")
@@ -30,13 +31,12 @@ def video_to_images(video_path, output_dir, file_name_prefix, output_fps):
             os.remove(file)
         print(f"Removed content of directory: {output_path}")
 
-    # Iterate through video - frame by frame
+    # Iterate through video and save images
     while video_capture.isOpened():
         video_capture.set(cv2.CAP_PROP_POS_MSEC, frame_count * 1000 / float(output_fps))
         has_frame, frame = video_capture.read()
         print(f"Processing frame of index: {frame_count:06d} / ~{output_frames_total:06d}. \
             Frame read status: {has_frame}")
-
         if has_frame:
             cv2.imwrite(os.path.join(output_path, f"{file_name_prefix}_{frame_count:06d}.jpg"), frame)
             frame_count += 1
