afe4f9d40ebc181d3243a8727b9208a5a5240ac4
Tom Iżycki <tmizycki@gmail.com>
2020-09-13 20:42:19 +0200

    Add alpha_pose, hand_render, hand_alpha_pose args

--- utils/keypoints_from_images.py
+++ utils/keypoints_from_images.py
@@ -7,7 +7,7 @@ import time
 OPENPOSE_PATH = '/sign-language/openpose/'
 
 
-def keypoints_from_images(image_dir, write_json, render_pose):
+def keypoints_from_images(image_dir, write_json, render_pose=-1, alpha_pose=0.5, hand_render=-1, hand_alpha_pose=0.5):
     try:
         sys.path.append(os.path.join(OPENPOSE_PATH, 'build/python'))
         from openpose import pyopenpose as op
@@ -19,6 +19,9 @@ def keypoints_from_images(image_dir, write_json, render_pose):
     params = dict()
     params['write_json'] = write_json
     params['render_pose'] = render_pose
+    params['alpha_pose'] = alpha_pose
+    params['hand_render'] = hand_render
+    params['hand_alpha_pose'] = hand_alpha_pose
     params['model_folder'] = os.path.join(OPENPOSE_PATH, 'models/')
     params["face"] = False
     params['hand'] = True
@@ -51,12 +54,19 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_dir", default='', help='Directory of input images')
     parser.add_argument('--write_json', default='', help='Directory to save keypoints')
-    parser.add_argument('--render_pose', default=0, help='Flag for rendering pose in output image')
+    parser.add_argument('--render_pose', default=-1, help='Flag for rendering pose in output image')
+    parser.add_argument('--alpha_pose', default=0.5, help='Alpha for rendering pose in output image')
+    parser.add_argument('--hand_render', default=-1, help='Flag for rendering hand in output image')
+    parser.add_argument('--hand_alpha_pose', default=0.5, help='Alpha for rendering hand in output image')
+
     args = parser.parse_known_args()
 
     # Get keypoints
     keypoints_from_images(
         args[0].image_dir,
         args[0].write_json,
-        args[0].render_pose
+        args[0].render_pose,
+        args[0].alpha_pose,
+        args[0].hand_render,
+        args[0].hand_alpha_pose
     )
