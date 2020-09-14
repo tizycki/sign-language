1dc9858ed014819c840edaf1cf686702aec7f755
Tom Iżycki <tmizycki@gmail.com>
2020-09-14 18:41:42 +0200

    Remove rendering pose option

--- utils/keypoints_from_images.py
+++ utils/keypoints_from_images.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 OPENPOSE_PATH = '/sign-language/openpose/'
 
 
-def keypoints_from_images(image_dir, write_json, render_pose=-1, alpha_pose=0.5, hand_render=-1, hand_alpha_pose=0.5):
+def keypoints_from_images(image_dir, write_json):
     try:
         sys.path.append(os.path.join(OPENPOSE_PATH, 'build/python'))
         from openpose import pyopenpose as op
@@ -19,15 +19,13 @@ def keypoints_from_images(image_dir, write_json, render_pose=-1, alpha_pose=0.5,
     # Params
     params = dict()
     params['write_json'] = write_json
-    params['render_pose'] = render_pose
-    params['alpha_pose'] = alpha_pose
-    params['hand_render'] = hand_render
-    params['hand_alpha_pose'] = hand_alpha_pose
+    params['display'] = 0
+    params['render_pose'] = 0
     params['model_folder'] = os.path.join(OPENPOSE_PATH, 'models/')
     params["face"] = False
     params['hand'] = True
     params['model_pose'] = 'BODY_25'
-    params['keypoint_scale'] = 3  # scales to range [0,1] where (0,0) is top-left corner
+    params['keypoint_scale'] = 0  # 3 -> scales to range [0,1] where (0,0) is top-left corner
 
     # Starting OpenPose
     op_wrapper = op.WrapperPython()
@@ -55,19 +53,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--image_dir", default='', help='Directory of input images')
     parser.add_argument('--write_json', default='', help='Directory to save keypoints')
-    parser.add_argument('--render_pose', default=-1, help='Flag for rendering pose in output image')
-    parser.add_argument('--alpha_pose', default=0.5, help='Alpha for rendering pose in output image')
-    parser.add_argument('--hand_render', default=-1, help='Flag for rendering hand in output image')
-    parser.add_argument('--hand_alpha_pose', default=0.5, help='Alpha for rendering hand in output image')
 
     args = parser.parse_known_args()
 
     # Get keypoints
     keypoints_from_images(
         args[0].image_dir,
-        args[0].write_json,
-        args[0].render_pose,
-        args[0].alpha_pose,
-        args[0].hand_render,
-        args[0].hand_alpha_pose
+        args[0].write_json
     )
