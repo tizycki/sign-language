69cb362f8395ca585985543e8224a6f33cc9b3d7
Tom Iżycki <tmizycki@gmail.com>
2020-09-17 18:32:44 +0200

    Add SequenceRecognitionNet and fix dropouts

--- model/models.py
+++ model/models.py
@@ -1,5 +1,4 @@
 from torch import nn
-import torch.nn.functional as F
 
 
 class StopPoseNet(nn.Module):
@@ -12,25 +11,64 @@ class StopPoseNet(nn.Module):
         self.fc3 = nn.Linear(512, 128)
         self.fc4 = nn.Linear(128, 32)
         self.fc5 = nn.Linear(32, 2)
+        self.dropout1 = nn.Dropout(0.1)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         output = x.view(x.size(0), -1)
 
         output = self.fc1(output)
-        output = F.relu(output)
-        F.dropout(output, 0.1)
+        output = self.relu(output)
+        self.dropout1(output)
 
         output = self.fc2(output)
-        output = F.relu(output)
-        F.dropout(output, 0.1)
+        output = self.relu(output)
+        self.dropout1(output)
 
         output = self.fc3(output)
-        output = F.relu(output)
-        F.dropout(output, 0.1)
+        output = self.relu(output)
+        self.dropout1(output)
 
         output = self.fc4(output)
-        output = F.relu(output)
-        F.dropout(output, 0.1)
+        output = self.relu(output)
+        self.dropout1(output)
+
+        output = self.fc5(output)
+
+        return output
+
+
+class SequenceRecognitionNet(nn.Module):
+
+    def __init__(self, out_feature_num):
+        super(SequenceRecognitionNet, self).__init__()
+
+        self.fc1 = nn.Linear(5700, 2048)
+        self.fc2 = nn.Linear(2048, 1024)
+        self.fc3 = nn.Linear(1024, 512)
+        self.fc4 = nn.Linear(512, 128)
+        self.fc5 = nn.Linear(128, out_feature_num)
+        self.dropout1 = nn.Dropout(0.1)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        output = x.view(x.size(0), -1)
+
+        output = self.fc1(output)
+        output = self.relurelu(output)
+        self.dropout1(output)
+
+        output = self.fc2(output)
+        output = self.relu(output)
+        self.dropout1(output)
+
+        output = self.fc3(output)
+        output = self.relu(output)
+        self.dropout1(output)
+
+        output = self.fc4(output)
+        output = self.relu(output)
+        self.dropout1(output)
 
         output = self.fc5(output)
 
