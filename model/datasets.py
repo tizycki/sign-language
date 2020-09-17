811410279fd59f024d2474615010b95495dd43a4
Tom Iżycki <tmizycki@gmail.com>
2020-09-17 18:30:49 +0200

    Add Dataset for sequence classification

--- model/datasets.py
+++ model/datasets.py
@@ -1,5 +1,6 @@
 from utils.keypoints import rescale_keypoints, read_keypoints
 from torch.utils.data import Dataset
+import numpy as np
 
 
 class OneClassKeypointClassificationDataset(Dataset):
@@ -14,9 +15,9 @@ class OneClassKeypointClassificationDataset(Dataset):
 
     def __getitem__(self, index):
         if self.keypoints_transform:
-            keypoints = self.load_from_disk(index)
-        else:
             keypoints = self.keypoints_transform(self.load_from_disk(index))
+        else:
+            keypoints = self.load_from_disk(index)
 
         target = self.load_target(index)
 
@@ -30,3 +31,35 @@ class OneClassKeypointClassificationDataset(Dataset):
     def load_target(self, index):
 
         return self.annotations.iloc[index]['stop_pose']
+
+
+class SequenceKeypointsDataset(Dataset):
+
+    def __init__(self, annotations, keypoints_transform=None):
+        super().__init__()
+        self.annotations = annotations
+        self.keypoints_transform = keypoints_transform
+
+    def __len__(self):
+        return self.annotations.shape[0]
+
+    def __getitem__(self, index):
+        if self.keypoints_transform:
+            keypoints_sequence = self.keypoints_transform(self.load_from_disk(index))
+        else:
+            keypoints_sequence = self.load_from_disk(index)
+
+        target = self.load_target(index)
+
+        return keypoints_sequence, target
+
+    def load_from_disk(self, index):
+        keypoints_sequence = []
+        for keypoints_path in self.annotations.iloc[index].keypoints_path:
+            keypoints = rescale_keypoints(read_keypoints(keypoints_path))
+            keypoints_sequence.append(keypoints)
+        return np.array(keypoints_sequence).reshape([len(keypoints_sequence), -1, 2])
+
+    def load_target(self, index):
+
+        return self.annotations.iloc[index].label_id
