9cb6d659b320e092707234d46fb3a45cc611a7e4
Tom Iżycki <tmizycki@gmail.com>
2020-09-17 18:30:00 +0200

    Introduce rotate keypoints sequence feature

--- utils/data_augmentation.py
+++ utils/data_augmentation.py
@@ -11,3 +11,14 @@ def rotate_keypoints(keypoints, min_angle, max_angle):
     rotated_keypoints[:, 1] = math.sin(angle) * keypoints[:, 0] + math.cos(angle) * keypoints[:, 1]
 
     return rescale_keypoints(rotated_keypoints)
+
+
+def rotate_keypoints_sequence(keypoints_sequence, min_angle, max_angle):
+    angle = math.radians(np.random.uniform(min_angle, max_angle))
+    rotated_keypoints_sequence = np.copy(keypoints_sequence)
+
+    rotated_keypoints_sequence[:, :, 0] = math.cos(angle) * keypoints_sequence[:, :, 0] - math.sin(angle) * keypoints_sequence[:, :, 1]
+    rotated_keypoints_sequence[:, :, 1] = math.sin(angle) * keypoints_sequence[:, :, 0] + math.cos(angle) * keypoints_sequence[:, :, 1]
+    rotated_keypoints_sequence[:] = rescale_keypoints(rotated_keypoints_sequence[:])
+
+    return np.array(rotated_keypoints_sequence).reshape(len(keypoints_sequence), -1, 2)
