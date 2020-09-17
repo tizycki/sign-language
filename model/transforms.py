044472a2541be7fc4298cf46ff03be3b3a7bb239
Tom Iżycki <tmizycki@gmail.com>
2020-09-17 18:33:15 +0200

    Add RotateKeypointsSequence transformer

--- model/transforms.py
+++ model/transforms.py
@@ -1,4 +1,5 @@
-from utils.data_augmentation import rotate_keypoints
+from utils.data_augmentation import rotate_keypoints, rotate_keypoints_sequence
+from utils.keypoints import keypoints_sequence_padding
 
 
 class RotateKeypoints(object):
@@ -9,3 +10,21 @@ class RotateKeypoints(object):
 
     def __call__(self, keypoints):
         return rotate_keypoints(keypoints, self.min_angle, self.max_angle)
+
+
+class RotateKeypointsSequence(object):
+
+    def __init__(self, min_angle, max_angle):
+        self.min_angle = min_angle
+        self.max_angle = max_angle
+
+    def __call__(self, keypoints_sequence):
+        return rotate_keypoints_sequence(keypoints_sequence, self.min_angle, self.max_angle)
+
+
+class KeypointsSequencePadding(object):
+    def __init__(self, output_length):
+        self.output_length = output_length
+
+    def __call__(self, keypoints_sequence):
+        return keypoints_sequence_padding(keypoints_sequence, self.output_length)
