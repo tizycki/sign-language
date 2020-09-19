315d74b0db0e57ee54687b5705532a17c18630b6
Tom Iżycki <tmizycki@gmail.com>
2020-09-19 11:59:29 +0200

    Add pose rendering

--- utils/openpose_wrapper.py
+++ utils/openpose_wrapper.py
@@ -8,11 +8,12 @@ from tqdm import tqdm
 OPENPOSE_PATH = '/sign-language/openpose/'
 
 
-def keypoints_from_images(image_dir, write_json):
+def keypoints_from_images(image_dir, write_json, write_images):
     """
     Use openpose tool to convert images into JSON files with keypoints
     :param image_dir: directory of input images
     :param write_json: directory of output JSON files with keypoints
+    :param write_images: directory of output images - with rendered skeleton
     :return:
     """
     # Set timer
@@ -29,13 +30,15 @@ def keypoints_from_images(image_dir, write_json):
     # Set openpose params
     params = dict()
     params['write_json'] = write_json
-    params['display'] = 0
-    params['render_pose'] = 0
+    params['write_images'] = write_images
     params['model_folder'] = os.path.join(OPENPOSE_PATH, 'models/')
+    params['model_pose'] = 'BODY_25'
     params["face"] = False
     params['hand'] = True
-    params['model_pose'] = 'BODY_25'
     params['keypoint_scale'] = 0  # 3 -> scales to range [0,1] where (0,0) is top-left corner
+    params['display'] = 0
+    params['render_pose'] = 2
+    params['alpha_pose'] = 0.5
 
     # Create openpose python wrapper
     op_wrapper = op.WrapperPython()
@@ -61,13 +64,15 @@ def keypoints_from_images(image_dir, write_json):
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_dir", default='', help='Directory of input images')
+    parser.add_argument("--image_dir", default='', help='Directory to input images')
     parser.add_argument('--write_json', default='', help='Directory to save keypoints')
+    parser.add_argument('--write_images', default='', help='Directory to save rendered images')
 
     args = parser.parse_known_args()
 
     # Get keypoints
     keypoints_from_images(
         args[0].image_dir,
-        args[0].write_json
+        args[0].write_json,
+        args[0].write_images
     )
